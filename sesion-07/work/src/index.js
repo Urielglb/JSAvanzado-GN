8571359b8068ca8558db1f5415035801d84a5165
Uriel García Luna Bobadilla <leiru06@MacBook-Pro-de-Uriel.local>
2022-11-02 21:55:09 -0600

    Sesion 06 Ejemplos

---
+++ sesion-07/work/src/index.js
@@ -0,0 +1,4 @@
+import {suma} from "./operation"
+import './app.css'
+
+console.log(`El resultado de la suma es:${suma(2,9)}`);
---
+++ sesion-07/work/src/operation.js
@@ -0,0 +1,5 @@
+const suma = (a,b) => a + b;
+
+export {
+    suma   
+}
