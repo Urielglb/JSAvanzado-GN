2c554f7df334463e767b77da9e1afc8e3bff63f3
Uriel García Luna Bobadilla <leiru06@MacBook-Pro-de-Uriel.local>
2022-11-03 16:15:26 -0600

    Ejemplos

---
+++ sesion-07/work/ejemplo-02/dist/bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var r=[0,1,!1,2,"",3],e=r.filter((function(r){return!!r}));console.log("\nHere is the original array: ".concat(r,"\nAnd here is the compacted array: ").concat(e,"\n"))})();
---
+++ sesion-07/work/ejemplo-02/src/util.js
@@ -0,0 +1,5 @@
+const compact = (array) => array.filter((element) => !!element)
+
+export{
+    compact
+}
